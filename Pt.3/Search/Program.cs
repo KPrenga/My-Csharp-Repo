b15a3ac258b5279e80ab91a1799066f9960618bb
KPrenga <klevisprenga23@gmail.com>
2025-06-11 17:04:55 +0100

    Modification of Search folder changes based on tutorial on: https://learn.microsoft.com/en-us/dotnet/csharp/tour-of-csharp/tutorials/hello-world#code-try-7 search section

--- Pt.3/Search/Program.cs
+++ Pt.3/Search/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string songLyrics = "You say goodbye, and I say hello";
+Console.WriteLine("Does Lyrics Contain goodbye: " + songLyrics.Contains("goodbye"));
+Console.WriteLine("Does Lyrics Contain greetings: " + songLyrics.Contains("greetings"));
+
+//dotnet learn search challenge:
+var youSearchStart = songLyrics.StartsWith("You");
+var goodbyeSearchStart = songLyrics.StartsWith("goodbye");
+var helloSearchEnd = songLyrics.EndsWith("hello");
+var goodbyeSearchEnd = songLyrics.EndsWith("goodbye");
+
+Console.WriteLine(
+    "Lyric Start\n" +
+    $"You: {youSearchStart}\n" +
+    $"goodbye: {goodbyeSearchStart}\n" +
+    "Lyric End \n" +
+    $"hello: {helloSearchEnd}\n" +
+    $"goodbye: {goodbyeSearchEnd}"
+    );
