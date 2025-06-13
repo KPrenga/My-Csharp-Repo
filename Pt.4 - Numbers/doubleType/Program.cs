8f624f42a7032da6eed905755a42e920b4b680e4
KPrenga <klevisprenga23@gmail.com>
2025-06-13 13:32:00 +0100

    doubleType edit

--- Pt.4 - Numbers/doubleType/Program.cs
+++ Pt.4 - Numbers/doubleType/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 
+double a = 19;
+double b = 23;
+double c = 8;
+double d = (a + b) / c;
+Console.WriteLine(d);
+
+//Min/Max of double float
+double max = double.MaxValue;
+double min = double.MinValue;
+Console.WriteLine($"The range of double is {min} to {max}");
+
+//rounding error example
+double third = 1.0 / 3.0;
+Console.WriteLine(third);
