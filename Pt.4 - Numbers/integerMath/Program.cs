1d89c00678dd97982c732a0250c53af2f64ecee1
KPrenga <klevisprenga23@gmail.com>
2025-06-13 13:04:28 +0100

    integerMath Edit

--- Pt.4 - Numbers/integerMath/Program.cs
+++ Pt.4 - Numbers/integerMath/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+int a = 18;
+int b = 6;
+int c = a + b;
+int d = a * b;
+int e = a - b;
+int f = a / b;
+
+Console.WriteLine(c);
+Console.WriteLine(d);
+Console.WriteLine(e);
+Console.WriteLine(f);
