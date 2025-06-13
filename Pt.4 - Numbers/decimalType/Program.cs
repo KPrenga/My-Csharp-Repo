dd17ffa1b8d394faaee80a83dcb9da86f7a983fa
KPrenga <klevisprenga23@gmail.com>
2025-06-13 13:59:50 +0100

    decimalType edit

--- Pt.4 - Numbers/decimalType/Program.cs
+++ Pt.4 - Numbers/decimalType/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//decimal min/max
+
+decimal min = decimal.MinValue;
+decimal max = decimal.MaxValue;
+Console.WriteLine($"The range of the decimal type is {min} to {max}");
+
+// decimal higher precision
+
+double a = 1.0;
+double b = 3.0;
+Console.WriteLine(a / b);
+
+decimal c = 1.0M;
+decimal d = 3.0M;
+Console.WriteLine(c / d);
+
+//C# Learn challenge
+
+double P = Math.PI;
+double r = 2.50;
+
+double Area = Math.Round(P * (r * r),3);
+
+Console.WriteLine($" area of circle  with radius {r} = {Area}");
