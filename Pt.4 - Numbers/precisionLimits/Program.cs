7faf45e7837b31f8f888695701e9cd14d6e83e50
KPrenga <klevisprenga23@gmail.com>
2025-06-13 13:20:52 +0100

    precisionLimits edit

--- Pt.4 - Numbers/precisionLimits/Program.cs
+++ Pt.4 - Numbers/precisionLimits/Program.cs
@@ -1,2 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+//example of int truncationg div result
+int a = 7;
+int b = 4;
+int c = 3;
+int d = (a + b) / c;
+//% to get remainder
+int e = (a + b) % c;
+Console.WriteLine($"quotient: {d}");
+Console.WriteLine($"remainder: {e}");
+
+//int range min/max
+
+int max = int.MaxValue;
+int min = int.MinValue;
+Console.WriteLine($"The range of integers ins {min} to {max}");
+
+//overflow example
+
+int overflow = max + 5;
+Console.WriteLine($"Overflow Example: {overflow}");
+
+//underflow example
+int underflow = min - 5;
+Console.WriteLine($"Underflow example: {underflow}");
+
+//int is circular
+//going past the limit sends you around, past the other limit 
