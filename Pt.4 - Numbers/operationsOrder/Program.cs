14d74c43f87f3c2987c8bc00b686cd9e7bf2c66c
KPrenga <klevisprenga23@gmail.com>
2025-06-13 13:11:26 +0100

    operationsOrder Edit

--- Pt.4 - Numbers/operationsOrder/Program.cs
+++ Pt.4 - Numbers/operationsOrder/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+int a = 5;
+int b = 4;
+int c = 2;
+int d = a + b * c;
+
+// order of operation is bidmas
+// multiplication before addition
+Console.WriteLine(d);
+//4 x 2 = 8
+//5 + 8 = 13
+
+d = (a + b) * c;
+Console.WriteLine(d);
+
+// a + b first then multiply by c
+// since brackets first
