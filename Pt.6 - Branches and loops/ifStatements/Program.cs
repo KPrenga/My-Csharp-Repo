4836c7af36f4fa14b40b001811efc2818b73f509
KPrenga <klevisprenga23@gmail.com>
2025-06-18 17:51:52 +0100

    addition of pt.6 and its contents: ifStatements folder

---
+++ Pt.6 - Branches and loops/ifStatements/Program.cs
@@ -0,0 +1,58 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Program
+{
+    public class Program
+    {
+
+        static int a = 5;
+        static int b = 3;
+        static int c = 4;
+
+
+        static void IfStatmentExample()
+        {
+            if (a + b > 10)
+            {
+                Console.WriteLine("The answer is greater than 10");
+            }
+            else
+            {
+                Console.WriteLine("The answer is not greater than 10");
+            }
+        }
+
+        static void IfStatmentExampleV2()
+        {
+            if ((a + b + c > 10) && (a == b))
+            {
+                Console.WriteLine("The answer is greater than 10");
+                Console.WriteLine("And the first number is equal to the second");
+            }
+            else
+            {
+                Console.WriteLine("The answer is not greater than 10");
+                Console.WriteLine("Or the first number is not equal to the second");  
+            }
+        }
+
+        static void ifStatementOr()
+        {
+             if ((a + b + c > 10) || (a == b))
+            {
+                Console.WriteLine("The answer is either greater than 10 or a and b are the same");
+            }
+            else
+            {
+                Console.WriteLine("The answer is not greater than 10");
+                Console.WriteLine("A and b are not the same");  
+            }
+        }
+
+        public static void Main()
+        {
+            //IfStatmentExample();
+            //IfStatmentExampleV2();
+            ifStatementOr();
+        }
+    }
+}
