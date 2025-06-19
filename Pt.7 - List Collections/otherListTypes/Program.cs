c80568a59a5794a9c223ec693aefec234da45252
KPrenga <klevisprenga23@gmail.com>
2025-06-19 19:41:05 +0100

    list types folder and its contents

---
+++ Pt.7 - List Collections/otherListTypes/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Threading.Tasks.Dataflow;
+
+namespace Program
+{
+    public class Program
+    {
+        static List<int> fibonacciNumbers = [1, 1];
+
+        
+
+        static void fibonacciExample()
+        {
+            var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
+            var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
+
+            fibonacciNumbers.Add(previous + previous2);
+
+            foreach (var item in fibonacciNumbers)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        static void fibonacciTwenty()
+        {
+
+            int counter = 1;
+            do
+            {
+                int lastNum = fibonacciNumbers[fibonacciNumbers.Count - 1] + fibonacciNumbers[fibonacciNumbers.Count - 2];
+                fibonacciNumbers.Add(lastNum);
+                counter++;
+
+            } while (counter <= 20);
+
+            foreach (var item in fibonacciNumbers)
+            {
+                Console.WriteLine($"{item}");
+            }
+        }
+        public static void Main(string[] args)
+        {
+
+            fibonacciTwenty();
+
+        }
+    }
+}
