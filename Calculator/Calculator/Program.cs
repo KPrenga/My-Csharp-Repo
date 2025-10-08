a1a5fa4eb5e3c00ea805c67d3a74a7c2dd14d063
KPrenga <klevisprenga23@gmail.com>
2025-10-08 15:01:36 +0100

    spliting calculator into two projects and creating dependencies between them

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -1,38 +1,7 @@
 ﻿using System.Text.RegularExpressions;
+using CalculatorLibrary.cs;
 
-class Calculator
-{
-    public static double DoCalculation(double num1, double num2, string op)
-    {
-        double result = double.NaN; // value set as no a number incase operation results in error
 
-        //switch for operation selection
-
-        switch (op)
-        {
-            case "a":
-                result = num1 + num2;
-                break;
-            case "s":
-                result = num1 - num2;
-                break;
-            case "m":
-                result = num1 * num2;
-                break;
-            case "d":
-                if (num2 != 0)
-                {
-                    result = num1 / num2;
-                }
-                break;
-            default:
-                Console.WriteLine("Please select a valid operation: ");
-                break;
-        }
-
-        return result;
-    }
-}
 class Program
 {
     static void Main(string[] args)
---
+++ Calculator/CalculatorLibrary.cs/CalculatorLibrary.cs
@@ -0,0 +1,37 @@
+﻿namespace CalculatorLibrary.cs;
+
+public class Calculator
+{
+
+    public static double DoCalculation(double num1, double num2, string op)
+    {
+        double result = double.NaN; // value set as no a number incase operation results in error
+
+        //switch for operation selection
+
+        switch (op)
+        {
+            case "a":
+                result = num1 + num2;
+                break;
+            case "s":
+                result = num1 - num2;
+                break;
+            case "m":
+                result = num1 * num2;
+                break;
+            case "d":
+                if (num2 != 0)
+                {
+                    result = num1 / num2;
+                }
+                break;
+            default:
+                Console.WriteLine("Please select a valid operation: ");
+                break;
+        }
+
+        return result;
+    }
+}
+
